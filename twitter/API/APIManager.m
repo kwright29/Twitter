e44a5621c052c2542afa00bd05ec68724996d5e9
kwright29 <khloewright329@gmail.com>
2022-06-20 15:05:40 -0700

    step 2 completed

--- twitter/API/APIManager.m
+++ twitter/API/APIManager.m
@@ -31,11 +31,12 @@ static NSString * const baseURLString = @"https://api.twitter.com";
     
     // TODO: fix code below to pull API Keys from your new Keys.plist file
     
-    NSString *path = [ [NSBundle mainBundle] pathForResource: @"Keys" ofType:@"plist"];
+    NSString *path = [[NSBundle mainBundle] pathForResource: @"Keys" ofType: @"plist"];
     NSDictionary *dict = [NSDictionary dictionaryWithContentsOfFile: path];
-    NSString *key = [dict objectForKey:@"consumer_Key"];
-    NSString *secret = [dict objectForKey:@"consumer_Secret"];
-    
+
+    NSString *key = [dict objectForKey: @"consumer_Key"];
+    NSString *secret = [dict objectForKey: @"consumer_Secret"];
+
     // Check for launch arguments override
     if ([[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"]) {
         key = [[NSUserDefaults standardUserDefaults] stringForKey:@"consumer-key"];

5fcd690c2bdf63b5ec6067dd70d7b531c4f5e0b0
kwright29 <khloewright329@gmail.com>
2022-06-21 16:07:15 -0700

    Configured Tweet Cells

--- twitter/View Controllers/TimelineViewController.m
+++ twitter/View Controllers/TimelineViewController.m
@@ -10,9 +10,13 @@
 #import "APIManager.h"
 #import "AppDelegate.h"
 #import "LoginViewController.h"
+#import "Tweet.h"
+#import "TweetCell.h"
+#import "UIImageView+AFNetworking.h"
 
-@interface TimelineViewController ()
-- (IBAction)didTapLogout:(id)sender;
+@interface TimelineViewController () <UITableViewDataSource, UITableViewDelegate>
+@property (strong, nonatomic) IBOutlet UITableView *tableView;
+
 @property (strong, nonatomic) NSMutableArray *arrayOfTweets;
 
 @end
@@ -21,26 +25,62 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    self.tableView.dataSource = self;
+    self.tableView.delegate = self;
     // Get timeline
     [[APIManager shared] getHomeTimelineWithCompletion:^(NSArray *tweets, NSError *error) {
         if (tweets) {
             NSLog(@"😎😎😎 Successfully loaded home timeline");
             // cast tweets to a mutable array
-            self.arrayOfTweets = (NSMutableArray *)tweets; //casting tweets to a mutable array
-            for (NSDictionary *dictionary in tweets) {
-                NSString *text = dictionary[@"text"];
+            //self.arrayOfTweets = [self tweetsWithArray:tweets]; //casting tweets to a mutable array
+            self.arrayOfTweets = (NSMutableArray *)tweets;
+            for (Tweet *tweet in tweets) {
+                //NSString *text = dictionary[@"text"];
+                NSString *text = tweet.text;
                 NSLog(@"%@", text);
             }
         } else {
             NSLog(@"😫😫😫 Error getting home timeline: %@", error.localizedDescription);
         }
+        [self.tableView reloadData];
     }];
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.arrayOfTweets.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    TweetCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"TweetCell"forIndexPath:indexPath];
+    
+    //getting singular tweet
+    Tweet *tweet = self.arrayOfTweets[indexPath.row];
+    //Tweet *newTweet = [self initWithDictionary:tweet];
+    
+    //setting labels etc
+    cell.displayName.text = tweet.user.name;
+    cell.username.text = tweet.user.screenName;
+    cell.createdDate.text = tweet.createdAtString;
+    cell.textOfTweet.text = tweet.text;
+    
+    //cell.createdDate.text =
+    
+    // to get profile picture
+    NSString *URLString = tweet.user.profilePicture;
+    NSURL *url = [NSURL URLWithString:URLString];
+    [cell.profilePic setImageWithURL:url];
+//    NSData *urlData = [NSData dataWithContentsOfURL:url];
+    
+    
+    return cell;
 }
 
 /*
@@ -55,11 +95,12 @@
 
 
 - (IBAction)didTapLogout:(id)sender {
-    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication];
+    AppDelegate *appDelegate = (AppDelegate *)[UIApplication sharedApplication].delegate;
     UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
     LoginViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
     appDelegate.window.rootViewController = loginViewController;
     
     [[APIManager shared] logout];
+
 }
 @end
---
+++ twitter/Views/TweetCell.h
@@ -0,0 +1,26 @@
+//
+//  TweetCell.h
+//  twitter
+//
+//  Created by Khloe Wright on 6/21/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TweetCell : UITableViewCell
+@property (strong, nonatomic) IBOutlet UIImageView *profilePic;
+@property (strong, nonatomic) IBOutlet UILabel *displayName;
+@property (strong, nonatomic) IBOutlet UILabel *username;
+@property (strong, nonatomic) IBOutlet UILabel *createdDate;
+@property (strong, nonatomic) IBOutlet UILabel *textOfTweet;
+- (IBAction)didTapRetweet:(id)sender;
+- (IBAction)didTapFavorite:(id)sender;
+- (IBAction)didTapReply:(id)sender;
+@property (strong, nonatomic) NSDictionary *tweet;
+
+@end
+
+NS_ASSUME_NONNULL_END

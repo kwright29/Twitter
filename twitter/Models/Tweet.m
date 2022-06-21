8c60d51d3d8e25a9aa743c0609f0050626cc4c31
kwright29 <khloewright329@gmail.com>
2022-06-21 09:44:11 -0700

    .gitignore Keys test

---
+++ twitter/Models/Tweet.m
@@ -0,0 +1,13 @@
+//
+//  Tweet.m
+//  twitter
+//
+//  Created by Khloe Wright on 6/20/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "Tweet.h"
+
+@implementation Tweet
+
+@end
---
+++ twitter/Models/User.m
@@ -0,0 +1,13 @@
+//
+//  User.m
+//  twitter
+//
+//  Created by Khloe Wright on 6/20/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "User.h"
+
+@implementation User
+
+@end

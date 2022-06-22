6988385da39e995128e3a381c81f7edfe5d228d2
kwright29 <khloewright329@gmail.com>
2022-06-22 13:49:16 -0700

    added ComposeViewController

---
+++ twitter/View Controllers/ComposeViewController.m
@@ -0,0 +1,38 @@
+//
+//  ComposeViewController.m
+//  twitter
+//
+//  Created by Khloe Wright on 6/22/22.
+//  Copyright © 2022 Emerson Malca. All rights reserved.
+//
+
+#import "ComposeViewController.h"
+
+@interface ComposeViewController ()
+@property (strong, nonatomic) IBOutlet UITextView *composeTweetText;
+
+@end
+
+@implementation ComposeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+- (IBAction)doesTapTweet:(id)sender {
+}
+- (IBAction)doesTapClose:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+}
+
+@end
